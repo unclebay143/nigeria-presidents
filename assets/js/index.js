f3c1367d682ad1aca778d2776830a7f362a38711
Prashantsetia <66926991+Prashantsetia@users.noreply.github.com>
2021-10-01 22:38:04 +0530

    Updated index.js for more info issue
    now user can see more info of presidents

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,14 +14,14 @@ const renderPresidentProfile = async (presidents) => {
   //  loop profile into temp container
   presidents.forEach((president) => {
     //
-    const { name, imgsrc, from, to, reason, causeofdeath, born, party } =
+    const { name, imgsrc, from, to, reason, causeofdeath, born, party, moreinfo } =
       president;
 
     const isReasionAvailable = reason ? `(${reason})` : "";
     const isDeceased = causeofdeath ? `(${causeofdeath})` : "";
     //
     temp += `
-    <div class="card" onclick="viewMore()">
+    <div class="card" onclick="viewMore(this)">
         <div class="card-image">
             <img
                 load="lazy"
@@ -31,7 +31,7 @@ const renderPresidentProfile = async (presidents) => {
                 height="100%"
             />
         </div>
-        <div class="read-more"><h3>read more</h3></div>
+        <div class="read-more"><a href="${moreinfo}"><h3>read more</h3></a></div>
         <div class="card-info">
             <h3 class="name">President ${name}</h3>
             <h3 class="tenure">From ${from} to ${to} ${isReasionAvailable}</h3>
@@ -65,10 +65,14 @@ const sortByOld = async () => {
   renderPresidentProfile(sortByOldest);
 };
 
-const viewMore = () => {
+const viewMore = (e) => {
   // TODO: users should be able to view more information about the president
-  document.querySelector(".message").innerText =
-    "Yes! you should be able to view more, you can fix this";
+  // document.querySelector(".message").innerText =
+  //   "Yes! you should be able to view more, you can fix this";
+  // var href = e.childNodes[3].innerHTML;
+  var href = e.children[1].children[0].getAttribute("href");
+
+  window.open(href, "_blank");
 };
 
 // Entry Level
