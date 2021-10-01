3d8d5548ccf8ff39e73a57f12479df0be00db6fe
unclebay <unclebigbay@gmail.com>
2021-10-01 09:33:55 +0100

    add: github star counter button fix: mobile resonsiveness fix: text

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,12 +7,9 @@ const fetchPresidentData = async () => {
 };
 
 // render the president profile to the ui
-const renderPresidentProfile = async () => {
+const renderPresidentProfile = async (presidents) => {
   // profile card parent div
   const profileCard = document.querySelector(".profile-cards");
-  // get the president from the fetch function
-  const { presidents } = await fetchPresidentData();
-  //
   let temp = "";
   //  loop profile into temp container
   presidents.forEach((president) => {
@@ -21,19 +18,23 @@ const renderPresidentProfile = async () => {
       president;
 
     const isReasionAvailable = reason ? `(${reason})` : "";
+    const isDeceased = causeofdeath ? `(${causeofdeath})` : "";
     //
     temp += `
-    <div class="card">
+    <div class="card" onclick="viewMore()">
         <div class="card-image">
             <img
+                load="lazy"
                 src="./assets/portraits/${imgsrc}"
                 alt="${name}"
+                width="100%"
+                height="100%"
             />
         </div>
         <div class="card-info">
             <h3 class="name">President ${name}</h3>
             <h3 class="tenure">From ${from} to ${to} ${isReasionAvailable}</h3>
-            <h3 class="born">Born: ${born}</h3>
+            <h3 class="born">Born: ${born} ${isDeceased}</h3>
             <h3 class="party">Party: ${party}</h3>
         </div>
     </div>
@@ -44,5 +45,26 @@ const renderPresidentProfile = async () => {
   profileCard.innerHTML = temp;
 };
 
-// entry function
-renderPresidentProfile();
+/* SORTING AREA*/
+const sortByLatest = async () => {
+  // Get the president from the fetch function
+  const { presidents } = await fetchPresidentData();
+  // Sort the president list from new to old
+  const sortByLatest = presidents.sort((a, b) => b.from - a.from);
+  renderPresidentProfile(sortByLatest);
+};
+
+/* SORTING AREA*/
+const sortByOld = async () => {
+  document.querySelector(".message").innerText =
+    "It is open source, fix this button";
+};
+
+const viewMore = () => {
+  // TODO: users should be able to view more information about the president
+  document.querySelector(".message").innerText =
+    "Yes! you should be able to view more, you can fix this";
+};
+
+// Entry Level
+sortByLatest();
