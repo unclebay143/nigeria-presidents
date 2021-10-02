26666f675986b8547d777b5540ea17b0d114d773
unknown <okorojiebube2@gmail.com>
2021-10-02 15:15:25 -0600

    added ernest shonekan profile, fixed sorting error by sorting against end of tenure due to sorting comflict from ernest shonekan profile

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,31 @@
 // fetch the json from file/endpoint
 const fetchPresidentData = async () => {
-  const response = await fetch("./assets/js/data.json");
-  const data = await response.json();
-  //
-  return data;
+	const response = await fetch('./assets/js/data.json');
+	const data = await response.json();
+	return data;
 };
 
 // render the president profile to the ui
-const renderPresidentProfile = async (presidents) => {
-  // profile card parent div
-  const profileCard = document.querySelector(".profile-cards");
-  let temp = "";
-  //  loop profile into temp container
-  presidents.forEach((president) => {
-    //
-    const { name, imgsrc, from, to, reason, causeofdeath, born, party } =
-      president;
+const renderPresidentProfile = async presidents => {
+	// profile card parent div
+	const profileCard = document.querySelector('.profile-cards');
+	let temp = '';
+	//  loop profile into temp container
+	presidents.forEach(president => {
+		//
+		const { name, imgsrc, from, to, reason, causeofdeath, born, party } = president;
 
-    const isReasionAvailable = reason ? `(${reason})` : "";
-    const isDeceased = causeofdeath ? `(${causeofdeath})` : "";
-    //
-    temp += `
+		const isReasionAvailable = reason ? `(${reason})` : '';
+		const isDeceased = causeofdeath ? `(${causeofdeath})` : '';
+		const tillDate = () => {
+			if (to == 2021) {
+				return 'Till Date';
+			} else {
+				return to;
+			}
+		};
+		//
+		temp += `
     <div class="card" onclick="viewMore()">
         <div class="card-image">
             <img
@@ -34,52 +39,52 @@ const renderPresidentProfile = async (presidents) => {
         <div class="read-more"><h3>read more</h3></div>
         <div class="card-info">
             <h3 class="name">President ${name}</h3>
-            <h3 class="tenure">From ${from} to ${to} ${isReasionAvailable}</h3>
+            <h3 class="tenure">From ${from} to ${tillDate()} ${isReasionAvailable}</h3>
             <h3 class="born">Born: ${born} ${isDeceased}</h3>
             <h3 class="party">Party: ${party}</h3>
         </div>
     </div>
   `;
-  });
+	});
 
-  // append temp to the profile card in the ui
-  profileCard.innerHTML = temp;
+	// append temp to the profile card in the ui
+	profileCard.innerHTML = temp;
 };
 
 /* SORTING AREA*/
 const sortByLatest = async () => {
-  // Get the president from the fetch function
-  const { presidents } = await fetchPresidentData();
-  // Sort the president list from new to old
-  const sortByLatest = presidents.sort((a, b) => b.from - a.from);
-  renderPresidentProfile(sortByLatest);
-  //setting Styles
-  setButtonStyle([true, false]);
+	// Get the president from the fetch function
+	const { presidents } = await fetchPresidentData();
+	// Sort the president list from new to old
+	const sortByLatest = presidents.sort((a, b) => b.to - a.to);
+	renderPresidentProfile(sortByLatest);
+	//setting Styles
+	setButtonStyle([true, false]);
 };
 
 /* SORTING AREA*/
 const sortByOld = async () => {
-  // Get the president from the fetch function
-  const { presidents } = await fetchPresidentData();
-  // Sort the president list from old to new
-  const sortByOldest = presidents.sort((a, b) => a.from - b.from);
-  // Render the president profile to the ui
-  renderPresidentProfile(sortByOldest);
-  //setting Styles
-  setButtonStyle([false, true]);
+	// Get the president from the fetch function
+	const { presidents } = await fetchPresidentData();
+	// Sort the president list from old to new
+	const sortByOldest = presidents.sort((a, b) => a.to - b.to);
+	// Render the president profile to the ui
+	renderPresidentProfile(sortByOldest);
+	//setting Styles
+	setButtonStyle([false, true]);
 };
 
-const setButtonStyle = (styles) => {
-  const buttons = document.querySelectorAll('.sorting-btn button');
-  buttons.forEach((button, index) => {
-    button.style.background = styles[index] ? 'green' : null;
-  });
+const setButtonStyle = styles => {
+	const buttons = document.querySelectorAll('.sorting-btn button');
+	buttons.forEach((button, index) => {
+		button.style.background = styles[index] ? 'green' : null;
+	});
 };
 
 const viewMore = () => {
-  // TODO: users should be able to view more information about the president
-  document.querySelector(".message").innerText =
-    "Yes! you should be able to view more, you can fix this";
+	// TODO: users should be able to view more information about the president
+	document.querySelector('.message').innerText =
+		'Yes! you should be able to view more, you can fix this';
 };
 
 // Entry Level
