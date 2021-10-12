5e9d7d2a88a5c4096259d000f05cc19556901b1c
Mrinmay Santra <mrinmay7875@gmail.com>
2021-10-12 15:56:50 +0530

    Configured Eslint and Prettier and refactored the code

--- assets/js/view-more.js
+++ assets/js/view-more.js
@@ -1,52 +1,46 @@
 // fetch the json from file/endpoint
 const fetchPresidentData = async () => {
-  const response = await fetch("./../assets/js/data.json");
-  const data = await response.json();
-  //
-  return data;
+    const response = await fetch("./../assets/js/data.json");
+    const data = await response.json();
+    //
+    return data;
+};
+
+const renderPresidentInformation = ({
+    name,
+    extraInformation,
+    imgsrc,
+    moreinfo,
+}) => {
+    const presidentNameContainer = document.getElementById("js-president-name");
+    const presidentImageHolder = document.getElementById("js-president-img");
+    const moreInfoContainer = document.getElementById("js-president-moreinfo");
+
+    presidentNameContainer.innerHTML = name;
+    moreInfoContainer.innerHTML = moreinfo || extraInformation[0].bio;
+    presidentImageHolder.setAttribute("src", `./../assets/portraits/${imgsrc}`);
 };
 
 const findSelectedPresident = async () => {
-  // grab clicked no
-  const presidentToView = JSON.parse(localStorage.getItem("viewing_president"));
+    // grab clicked no
+    const presidentToView = JSON.parse(
+        localStorage.getItem("viewing_president")
+    );
 
-  // grab president from json
-  const { presidents } = await fetchPresidentData();
+    // grab president from json
+    const { presidents } = await fetchPresidentData();
 
-  // find clicked no from json
-  const selectedPresident = presidents.filter(
-    (president) => president.no === presidentToView
-  );
+    // find clicked no from json
+    const selectedPresident = presidents.filter(
+        (president) => president.no === presidentToView
+    );
 
-  console.log(selectedPresident[0]);
+    console.log(selectedPresident[0]);
 
-  //display to ui
-  renderPresidentInformation(selectedPresident[0]);
+    // display to ui
+    renderPresidentInformation(selectedPresident[0]);
 };
 findSelectedPresident();
 // // localStorage.removeItem('name');
 // // localStorage.removeItem('imgsrc');
 // // localStorage.removeItem('info');
-
-const renderPresidentInformation = ({
-  name,
-  no,
-  born,
-  diedInOffice,
-  party,
-  extra_information,
-  contributors,
-  imgsrc,
-  tribe,
-  from,
-  to,
-  moreinfo,
-}) => {
-  var presidentNameContainer = document.getElementById("js-president-name");
-  var presidentImageHolder = document.getElementById("js-president-img");
-  var moreInfoContainer = document.getElementById("js-president-moreinfo");
-
-  presidentNameContainer.innerHTML = name;
-  moreInfoContainer.innerHTML = moreinfo || extra_information[0].bio;
-  presidentImageHolder.setAttribute("src", `./../assets/portraits/${imgsrc}`);
-};
